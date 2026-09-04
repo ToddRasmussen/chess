018fd2e8e49f25d3c77c13c366cd1f76cf293f48
ToddRasmussen <toddr64@byu.edu>
2026-09-04 08:10:58 -0600

    implemented move

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -7,24 +7,31 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
-    //TODO: Data
+
+    //DATA
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
-        //TODO: Initalize
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented"); //TODO
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented"); //TODO
+        return endPosition;
     }
 
     /**
@@ -34,6 +41,6 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented"); //TODO
+        return promotionPiece;
     }
 }
