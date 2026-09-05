128c049ba2c4d26ef71ed624cc237b8cb60b8749
ToddRasmussen <toddr64@byu.edu>
2026-09-05 12:58:39 -0600

    setup for phase 3

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,10 +1,11 @@
 package server;
 
-import chess.*;
-
 public class ServerMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        var port = 9000;
+        Server server = new Server();
+        server.run(port);
+
+        System.out.println("♕ 240 Chess Server running on port " + port);
     }
 }
