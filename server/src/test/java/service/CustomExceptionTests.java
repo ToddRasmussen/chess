efe7882f41f4f3cfce4c16f045147f9f2186940d
ToddRasmussen <toddr64@byu.edu>
2026-09-13 16:46:43 -0600

    expanded tests for Exceptions, and trying to get pom.xml to work maybe?

--- server/src/test/java/service/CustomExceptionTests.java
+++ server/src/test/java/service/CustomExceptionTests.java
@@ -7,29 +7,33 @@ public class CustomExceptionTests {
 
     @Test
     public void testAlreadyTakenException() {
-        AlreadyTakenException exMsg = new AlreadyTakenException("Already taken");
-        Assertions.assertNotNull(exMsg);
-        Assertions.assertEquals("Already taken", exMsg.getMessage());
+        AlreadyTakenException ex1 = new AlreadyTakenException("Already taken");
+        AlreadyTakenException ex2 = new AlreadyTakenException("Already taken", new Throwable());
+        Assertions.assertNotNull(ex1);
+        Assertions.assertNotNull(ex2);
     }
 
     @Test
     public void testDoesNotExistException() {
-        DoesNotExistException exMsg = new DoesNotExistException("Does not exist");
-        Assertions.assertNotNull(exMsg);
-        Assertions.assertEquals("Does not exist", exMsg.getMessage());
+        DoesNotExistException ex1 = new DoesNotExistException("Does not exist");
+        DoesNotExistException ex2 = new DoesNotExistException("Does not exist", new Throwable());
+        Assertions.assertNotNull(ex1);
+        Assertions.assertNotNull(ex2);
     }
 
     @Test
     public void testIncorrectPasswordException() {
-        IncorrectPasswordException exMsg = new IncorrectPasswordException("Incorrect password");
-        Assertions.assertNotNull(exMsg);
-        Assertions.assertEquals("Incorrect password", exMsg.getMessage());
+        IncorrectPasswordException ex1 = new IncorrectPasswordException("Incorrect password");
+        IncorrectPasswordException ex2 = new IncorrectPasswordException("Incorrect password", new Throwable());
+        Assertions.assertNotNull(ex1);
+        Assertions.assertNotNull(ex2);
     }
 
     @Test
     public void testInvalidAuthorizationException() {
-        InvalidAuthorizationException exMsg = new InvalidAuthorizationException("Invalid authorization");
-        Assertions.assertNotNull(exMsg);
-        Assertions.assertEquals("Invalid authorization", exMsg.getMessage());
+        InvalidAuthorizationException ex1 = new InvalidAuthorizationException("Invalid authorization");
+        InvalidAuthorizationException ex2 = new InvalidAuthorizationException("Invalid authorization", new Throwable());
+        Assertions.assertNotNull(ex1);
+        Assertions.assertNotNull(ex2);
     }
 }
